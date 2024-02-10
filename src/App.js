af3a9f941a98eeb18db8f1caf55da6b190dde117
threeBearsWithBike <trois@list.ru>
2024-02-10 17:13:30 +0300

    create component Timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Timer from './components/timer/Timer';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Timer />
     </div>
   );
 }
---
+++ src/components/timer/Timer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+    return (
+        <div>test component Timer</div>
+    )
+}
+
+export default Timer;
