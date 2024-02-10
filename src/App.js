8d7ead0c1113c9e168b7bbb4ca7d96f6b462f936
threeBearsWithBike <trois@list.ru>
2024-02-10 17:50:17 +0300

    timer is working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Timer from './components/timer/Timer';
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <Timer minutes = { 1 } seconds = { 10 } />
     </div>
   );
 }
--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-const Timer = () => {
+const Timer = ({ hours = 0, minutes = 0, seconds = 0}) => {
+    const [over, setOver] = useState(false);
+    const [[h, m, s], setTime] = useState([hours, minutes, seconds]);
+
+    const tick = () => {
+        if (over) {
+            return;
+        }
+
+        if (h === 0 && m === 0 && s === 0) {
+            setOver(true);
+        } else if (m === 0 && s === 0) {
+            setTime([h - 1, m = 59, s = 59]);
+        } else if (s === 0) {
+            setTime([h, m - 1, 59]);
+        } else {
+            setTime([h, m, s - 1]);
+        }
+    }
+
+    useEffect(() => {
+        let timerID = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerID);
+    })
     return (
-        <div>test component Timer</div>
+        <div>
+            {
+                `${h.toString().padStart(2, 0)} : 
+                ${m.toString().padStart(2, 0)} :
+                ${s.toString().padStart(2,0)}`
+            }
+        </div>
     )
 }
 
